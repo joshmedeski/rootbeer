6e30e5d0aa5f490db4bb9800f769a61b6038c66b
Josh Medeski <josh.medeski@gmail.com>
2013-09-21 18:39:22 -0500

    add title to top bar, make navigation right

--- components/topbar.php
+++ components/topbar.php
@@ -12,9 +12,9 @@
 <div class="contain-to-grid">
   <!-- Starting the Top-Bar -->
   <nav class="top-bar">
-      <ul class="title-area show-for-small">
+      <ul class="title-area">
           <li class="name">
-            <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="hide-for-small"><?php bloginfo( 'name' ); ?></a></h1>
+            <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
           </li>
       <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
       <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
@@ -25,7 +25,7 @@
               'theme_location' => 'topbar',
               'container' => false,
               'depth' => 0,
-              'items_wrap' => '<ul class="left">%3$s</ul>',
+              'items_wrap' => '<ul class="right">%3$s</ul>',
               'fallback_cb' => 'reverie_menu_fallback', // workaround to show a message to set up a menu
               'walker' => new rootbeer_walker( array(
                   'in_top_bar' => true,
@@ -33,10 +33,6 @@
               ) ),
           ) );
       ?>
-      <ul class="right">
-        <li class="divider hide-for-small"></li>
-        <li class="has-form"><?php get_search_form(); ?></li>
-      </ul>
       </section>
   </nav>
   <!-- End of Top-Bar -->
