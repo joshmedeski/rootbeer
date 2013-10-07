9396dc5c2add22284014b1f49c3f25ad19c009c3
Josh Medeski <josh@medeskidesign.com>
2013-10-07 15:57:26 -0500

    updated compass config file

--- lib/rootbeer.rb
+++ lib/rootbeer.rb
@@ -1,5 +1,13 @@
+root = File.join(File.dirname(__FILE__), "..")
 require "rootbeer/version"
 
 module Rootbeer
   # Your code goes here...
+end
+
+if defined?(Compass)
+  Compass::Frameworks.register("rootbeer",
+    :stylesheets_directory => File.join(root,"scss"),
+    :templates_directory => File.join(root,"templates")
+  )
 end
