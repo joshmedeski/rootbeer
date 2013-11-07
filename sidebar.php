9019ce464d757643094e4183c0e2bf1bb99eae3c
Josh <josh.medeski@gmail.com>
2013-11-07 17:18:52 -0600

    clean up php code in sidebar

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,7 @@
  * @framework  Foundation
  * @author     JoshMedeski
  */
-if ( is_active_sidebar( 'rootbeer_sidebar_widgets' ) ) :
-	dynamic_sidebar( 'rootbeer_sidebar_widgets' );
-endif; ?>
+?>
+	<?php if ( is_active_sidebar( 'sidebar-widgets' ) ) :
+		dynamic_sidebar( 'sidebar-widgets' );
+	endif; ?>
