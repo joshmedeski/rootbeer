c84829a38faffe61edfbe15a3faa4cd72d4dca28
Josh <josh.medeski@gmail.com>
2013-11-07 17:10:47 -0600

    remove page and single php, included in index, styling will be used in "content" files

--- index.php
+++ index.php
@@ -9,21 +9,21 @@
  */
 get_header(); ?>
 
-<!-- TODO: Intro CTA here, mailchimp signup -->
-
-<div class="row">
-    <!-- Main Content -->
-    <div class="large-8 columns main-content">
-        <?php if ( have_posts() ) : ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'content', get_post_format() ); ?>
-            <?php endwhile; ?>
-        <?php else : ?>
-            
-            <h2>Nothing here, come back soon!</h2>
-        <?php endif; // end have_posts() check ?>
+<section class="site__content">
+    <div class="row">
+        <main role="main" class="site__main large-8 columns">
+            <?php if ( have_posts() ) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php get_template_part( 'content', get_post_format() ); ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <?php get_template_part( 'content', 'none' ); ?>
+            <?php endif; ?>
+        </main>
+        <aside role="complementary" class="site__sidebar large-4 columns">
+            <?php get_sidebar(); ?>
+        </aside>
     </div>
-    <?php get_sidebar(); ?>
-</div>
+</section>
 
 <?php get_footer(); ?>
