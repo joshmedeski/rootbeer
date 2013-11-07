f8123ed19a796af18f7407f19f9a82958b4e7b26
Josh <josh.medeski@gmail.com>
2013-11-07 17:17:57 -0600

    update footer items, including adding wp_footer!

--- footer.php
+++ footer.php
@@ -6,31 +6,35 @@
  * @framework  Foundation
  * @author     JoshMedeski
  */
-wp_footer(); ?>
+?>
 
-<?php if ( is_active_sidebar( 'footer-widgets' ) ) :
-	dynamic_sidebar( 'footer_navigation' );
-endif; ?>
+<section class="site_bottom">
+	<?php if ( is_active_sidebar( 'bottom-widgets' ) ) :
+		dynamic_sidebar( 'bottom-widgets' );
+	endif; ?>
+</section>
 
-<footer class="footer">
-	<div class="footer__row">
+<footer class="site__footer">
+	<div class="row">
 		<?php 
 			$footer_navigation = array(
 				'theme_location'  => 'footer',
 				'container'       => 'div',
 				'container_class' => 'footer__nav',
-				'menu_class'      => 'nav--footer',
+				'menu_class'      => 'inline-list',
 				'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
 				'depth'           => 1,
 				'echo'            => true,
 				'fallback_cb'     => 'false'
 			); 
 		wp_nav_menu( $footer_navigation ); ?>
-		<div class="footer__colophon">
+		<div class="footer__copyright">
 			<p>&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?>.</p>
 		</div>
 		</div>
 </footer>
+
+<?php wp_footer(); ?>
 
 <script>
 	$(document).foundation();
