5cb9be23442adb807d6f3429163078e1708dae14
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-11-22 10:22:53 -0600

    update content layout (removed html comments)

--- content-page.php
+++ content-page.php
@@ -8,17 +8,18 @@
  * @framework  Foundation
  */
 ?>
-			<h2><?php the_title(); ?></h2>
 
-			<p><?php the_post_thumbnail(); ?></p>
+<h2><?php the_title(); ?></h2>
+
+<p><?php the_post_thumbnail(); ?></p>
 	
-			<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-		<?php else : ?>
-		<div class="entry-content">
+<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div>
+<?php else : ?>
+	<div class="entry-content">
 		<?php the_content( __( 'Read More &raquo;', 'rootbeer' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'rootbeer' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-		<?php endif; ?>
+	</div>
+<?php endif; ?>
