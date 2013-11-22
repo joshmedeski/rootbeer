ab48bcd6c4cf0ec059a7adb3062067f5f3b09428
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-11-22 11:44:28 -0600

    compile stylesheets to root

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "/"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
