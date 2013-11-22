80b21073e30374cf69e7996c3ff96e45a122f965
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-11-22 10:56:44 -0600

    cleanup spacing and extra php

--- index.php
+++ index.php
@@ -1,29 +1,18 @@
-<?php
-/**
- * The page for displaying the Index.
- *
- * @package    WordPress
- * @subpackage RootBeer
- * @author     JoshMedeski
- * @framework  Foundation
- */
-get_header(); ?>
-
-<section class="site__content">
+<?php get_header(); ?>
+  <section class="content">
     <div class="row">
-        <main role="main" class="site__main large-8 columns">
-            <?php if ( have_posts() ) : ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <?php get_template_part( 'content', get_post_format() ); ?>
-                <?php endwhile; ?>
-            <?php else : ?>
-                <?php get_template_part( 'content', 'none' ); ?>
-            <?php endif; ?>
-        </main>
-        <aside role="complementary" class="site__sidebar large-4 columns">
-            <?php get_sidebar(); ?>
-        </aside>
+      <main class="large-8 columns" role="main">
+        <?php if ( have_posts() ) :
+          while ( have_posts() ) : the_post();
+            get_template_part( 'content', get_post_format() );
+          endwhile;
+          else :
+            get_template_part( 'content', 'none' );
+        endif; ?>
+      </main>
+      <aside class="large-4 columns" role="complementary">
+        <?php get_sidebar(); ?>
+      </aside>
     </div>
-</section>
-
+  </section>
 <?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,3 @@
-<?php
-/* Sidebar
- *
- * @package    WordPress
- * @subpackage RootBeer
- * @framework  Foundation
- * @author     JoshMedeski
- */
-?>
-	<?php if ( is_active_sidebar( 'sidebar-widgets' ) ) :
-		dynamic_sidebar( 'sidebar-widgets' );
-	endif; ?>
+<?php if ( is_active_sidebar( 'sidebar-widgets' ) ) :
+	dynamic_sidebar( 'sidebar-widgets' );
+endif; ?>
