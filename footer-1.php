786bde7a9f56a693d2eeb9376c13ee3b79ef05fb
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-18 16:38:58 -0600

    add footer partials

---
+++ footer-1.php
@@ -0,0 +1,11 @@
+<?php get_sidebar('bottom'); ?>
+<footer class="footer">
+  <div class="row">
+  <div class="large-12 columns">
+  <?php get_template_part( 'nav-secondary', 'inlinelist' ); ?>
+      <div class="footer-copyright">
+        <p>&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?>.</p>
+      </div>
+    </div>
+  </div>
+</footer>
--- footer.php
+++ footer.php
@@ -1,33 +1,5 @@
-		<section class="row">
-			<?php if ( is_active_sidebar( 'bottom-widgets' ) ) :
-				dynamic_sidebar( 'bottom-widgets' );
-			endif; ?>
-		</section>
-		
-		<footer class="footer">
-			<div class="row">
-			<div class="large-12 columns">
-				<?php 
-					$footer_navigation = array(
-						'theme_location'  => 'footer',
-						'container'       => 'div',
-						'container_class' => 'footer__nav',
-						'menu_class'      => 'inline-list',
-						'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
-						'depth'           => 1,
-						'echo'            => true,
-						'fallback_cb'     => 'false'
-					);
-				wp_nav_menu( $footer_navigation ); ?>
-					<div class="footer__copyright">
-						<p>&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?>.</p>
-					</div>
-				</div>
-			</div>
-		</footer>
-
+		<?php get_template_part( 'footer', '1' ); ?>
 		<?php wp_footer(); ?>
-
 		<script>
   		$(document).foundation();
 		</script>
