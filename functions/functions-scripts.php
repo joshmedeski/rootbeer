cbc06dd9331cb70a576922a3f4747291fe36bd87
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-18 12:46:51 -0600

    update foundation scripts (with versions)

--- functions/functions-scripts.php
+++ functions/functions-scripts.php
@@ -7,30 +7,40 @@
  * @author     JoshMedeski
  */
 
+// Remove jQuery
+// wp_deregister_script('jquery');
+
+// Add Files
+add_action( 'wp_enqueue_scripts', 'rootbeer_modernizer_script' );
+add_action( 'wp_enqueue_scripts', 'rootbeer_jquery_script' );
+add_action( 'wp_enqueue_scripts', 'rootbeer_foundation_script' );
+
+// Fuctions
 function rootbeer_modernizer_script() {
 	wp_enqueue_script(
 		'modernizer',
-		get_template_directory_uri() . '/js/modernizer.js'
-	);
-}
-add_action( 'wp_enqueue_scripts', 'rootbeer_modernizer_script' );
-
-function rootbeer_jquery_script() {
-	wp_enqueue_script(
-		'jquery',
-		get_template_directory_uri() . '/js/jquery.min.js',
+		get_template_directory_uri() . '/js/modernizr.js',
+		"2.7.1",
 		array(),
 		true
 	);
 }
-// add_action( 'wp_enqueue_scripts', 'rootbeer_jquery_script' );
-
-function rootbeer_foundation_script() {
+function rootbeer_jquery_script() {
 	wp_enqueue_script(
-		'foundation',
-		get_template_directory_uri() . '/js/foundation.min.js',
+		'jquery',
+		get_template_directory_uri() . '/js/jquery.js',
+		"2.0.3",
+		array(),
 		true
 	);
 }
-// add_action( 'wp_enqueue_scripts', 'rootbeer_foundation_script' );
+function rootbeer_foundation_script() {
+	wp_enqueue_script(
+		'foundation',
+		get_template_directory_uri() . '/js/foundation.js',
+		"5.0.2",
+		array(),
+		true
+	);
+}
 ?>
