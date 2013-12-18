d64a63b196e87a27cca67a59621b3fd0923c3ba6
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-18 09:21:25 -0600

    update css and js directories in config file

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ add_import_path "bower_components/rootbeer/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/"
+css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
