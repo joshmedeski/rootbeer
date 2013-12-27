fc0d855a8d03710a32370d7d2122b6809ff2e3aa
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-27 17:07:32 -0600

    update get template part, change alert class

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'roots'); ?>
+  <div class="alert-box">
+    <?php _e('Sorry, no results were found.', 'rootbeer'); ?>
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('content', get_post_format()); ?>
+  <?php get_template_part('templates/content', get_post_format()); ?>
 <?php endwhile; ?>
