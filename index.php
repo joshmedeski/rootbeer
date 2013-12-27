b06b923c627c2831de7017c9edb9557291ccb96c
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-26 23:49:51 -0600

    remove includes

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<?php get_header(); ?>
-  <section class="content">
-    <div class="row">
-      <main class="large-8 columns" role="main">
-        <?php if ( have_posts() ) :
-          while ( have_posts() ) : the_post();
-            get_template_part( 'content', get_post_format() );
-          endwhile;
-          else :
-            get_template_part( 'content', 'none' );
-        endif; ?>
-      </main>
-      <?php get_sidebar(); ?>
-    </div>
-  </section>
-<?php get_footer(); ?>
+<section class="content">
+      <?php if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+          get_template_part( 'content', get_post_format() );
+        endwhile;
+        else :
+          get_template_part( 'content', 'none' );
+      endif; ?>
+</section>
