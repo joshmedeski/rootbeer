90dcd2d8a2403d05464044a7f7f94c2c8a041f90
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-27 17:07:16 -0600

    remove header and footer from page template

--- page-full.php
+++ page-full.php
@@ -6,16 +6,16 @@
  * @framework  Foundation
  * @author     JoshMedeski
  */
-get_header(); ?>
-  <section class="content">
-  	<div class="row">
-  		<main class="large-10 large-centered columns" role="main">
-  			<?php if ( have_posts() ) :
-  				while ( have_posts() ) : the_post();
-  					get_template_part( 'content', get_post_format() );
-  				endwhile;
-  			endif;?>
-  		</main>
-  	</div>
-  </section>
-<?php get_footer(); ?>
+?>
+
+<section class="content">
+	<div class="row">
+		<main class="large-10 large-centered columns" role="main">
+			<?php if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', get_post_format() );
+				endwhile;
+			endif;?>
+		</main>
+	</div>
+</section>
