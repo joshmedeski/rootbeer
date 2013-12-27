d45d716a3b08b9721acb745a17ee7a79c25f4176
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-26 23:48:19 -0600

    add alert-box class to IE alert

--- header.php
+++ header.php
@@ -12,10 +12,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+  
   <!--[if lt IE 8]>
-    <div class="alert alert-warning">
+    <div class="alert-box">
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
   <![endif]-->
   
-  <?php get_template_part( 'header', '1' ); ?>
+<?php get_template_part( 'header', '1' ); ?>
