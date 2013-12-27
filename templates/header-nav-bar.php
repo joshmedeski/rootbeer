cbecd8528a84d5a83a6663b73e372b935836969b
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-27 15:35:36 -0600

    move header styles to templates folder

--- templates/header-nav-bar.php
+++ templates/header-nav-bar.php
@@ -1,10 +1,10 @@
 <header class="header" role="banner">
   <div class="row">
-    <div class="large-8 columns">
+    <div class="medium-8 columns">
       <h1 class="site-title"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
       <p class="site-tagline subheader"><?php bloginfo('description'); ?></p>
     </div>
-    <div class="large-4 columns hide-for-small">
+    <div class="medium-4 columns hide-for-small">
       <?php get_search_form(); ?>
     </div>
   </div>
