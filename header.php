9d23f86b438b9ca134c4c831685e5b79fa6c64a0
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-26 23:56:11 -0600

    update app stylesheet reference

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php get_template_part( 'head', 'title' ); ?>
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/app.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.css">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php wp_head(); ?>
