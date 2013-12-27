f0421f66748355c567fa754772750c582831c783
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-26 23:17:11 -0600

    re-order functions

--- functions.php
+++ functions.php
@@ -9,8 +9,8 @@
 // Rootbeer Includes
 include_once('function/setup.php');
 include_once('function/wrapper.php');
-include_once('function/customize.php');
-include_once('function/styles.php');
 include_once('function/scripts.php');
-include_once('function/widgets.php');
+include_once('function/styles.php');
 include_once('function/menus.php');
+include_once('function/widgets.php');
+include_once('function/customize.php');
