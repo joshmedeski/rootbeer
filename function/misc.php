6ce4e701341e5edadbac9dd60b951a34fcd91841
Josh Medeski <joshmedeski@users.noreply.github.com>
2013-12-28 16:04:39 -0600

    add flex video wrap

---
+++ function/misc.php
@@ -0,0 +1,9 @@
+<?php
+
+// Flex Video Wrapping
+add_filter('embed_oembed_html', 'my_embed_oembed_html', 99, 4);
+function my_embed_oembed_html($html, $url, $attr, $post_id) {
+  return '<div class="flex-video">' . $html . '</div>';
+}
+
+?>
--- functions.php
+++ functions.php
@@ -13,4 +13,5 @@ include_once('function/scripts.php');
 include_once('function/styles.php');
 include_once('function/menus.php');
 include_once('function/widgets.php');
+include_once('function/misc.php');
 include_once('function/customize.php');
