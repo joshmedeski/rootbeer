6be5884a7565db640a5afde80a416eba65ca8875
Josh Medeski <joshmedeski@users.noreply.github.com>
2014-02-01 23:05:49 -0600

    remove icon fonts and social images

--- assets/function/misc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Flex Video Wrapping
-add_filter('embed_oembed_html', 'my_embed_oembed_html', 99, 4);
-function my_embed_oembed_html($html, $url, $attr, $post_id) {
-  return '<div class="flex-video">' . $html . '</div>';
-}
-
-?>
